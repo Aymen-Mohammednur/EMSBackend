6a3dd57261663d7b23b537672fedb45ed495b2f5
Semere Habtu <se.semere.habtu@gmail.com>
2021-06-01 19:10:20 +0300

    Put and delete requests

--- ems/bonus_cuts.py
+++ ems/bonus_cuts.py
@@ -28,6 +28,40 @@ class BonusCutsAPI(Resource):
         else:
             bonus = BonusCuts.query.all()
             return bonus
+    
+    def put(self, bonus_id):
+        bonus_cuts = BonusCuts.query.filter_by(id=bonus_id).first()
+        if bonus_cuts:
+            if request.is_json:
+                bonus_date = request.json['date']
+                bonus_amount = request.json['amount']
+                bonus_remark = request.json['remark']
+                bonus_employee_id = request.json['employee_id']
+            else:
+                bonus_date = request.form['date']
+                bonus_amount = request.form['amount']
+                bonus_remark = request.form['remark']
+                bonus_employee_id = request.form['employee_id']
+
+
+            bonus_cuts.date = bonus_date
+            bonus_cuts.amount = bonus_amount
+            bonus_cuts.remark = bonus_remark
+            bonus_cuts.employee_id = bonus_employee_id
+
+            db.session.commit()
+
+        else:
+            abort(404, "No bonus/cut with that Id")
+
+    def delete(self, bonus_id):
+        bonus_cuts = BonusCuts.query.filter_by(id=bonus_id).first()
+        if bonus_cuts:
+            db.session.delete(bonus_cuts)
+            db.session.commit()
+            return jsonify(message="Bonus/Cut entry successfully deleted"), 200
+        else:
+            abort(404, "No bonus/cuts with that Id")
 
             
     
