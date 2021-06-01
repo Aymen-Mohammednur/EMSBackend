4f44524fcda6c32dbbb5faf0c934ff2fbfa3d0a2
betziii <se.bethelhem.teshibelay@gmail.com>
2021-06-01 05:32:54 +0300

    schemas for departement, attendance, bonus, salary

--- schemas.py
+++ schemas.py
@@ -19,3 +19,38 @@ class EmployeeSchema(ma.Schema):
     date_of_birth = fields.String(required=True)
     hourly_rate = fields.Float(required=True)
     department_id = fields.Integer(required=True)
+class DepartmentSchema(ma.Schema):
+    class Meta:
+        ordered = True
+        fields = ('id', 'department_title', 'no_of_employees')
+    no_of_employees = fields.Integer(required=True)
+    department_title = fields.String(required=True)
+    
+
+
+class AttendanceSchema(ma.Schema):
+    class Meta:
+        ordered = True
+        fields = ('id', 'work_time', 'employee_id', 'date')
+    work_time = fields.Float(required=True)
+    employee_id = fields.Integer(required=True)
+    date = fields.String(required=True)
+
+class BonusSchema(ma.Schema):
+    class Meta:
+        ordered = True
+        fields = ('id', 'date', 'amount', 'remark', 'employee_id')
+    date = fields.String(required=True)
+    amount = fields.Float(required=True)
+    remark = fields.String(required=True)
+    employee_id = fields.Integer(required=True)
+
+class SalarySchema(ma.Schema):
+    class Meta:
+        ordered = True
+        fields = ('id', 'date', 'amount', 'tax', 'net', 'employee_id')
+    date = fields.String(required=True)
+    amount = fields.Float(required=True)
+    tax = fields.Float(required=True)
+    net = fields.String(required=True)
+    employee_id = fields.Integer(required=True)
