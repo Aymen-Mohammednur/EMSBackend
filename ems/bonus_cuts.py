41d3850d2a510e3da1f9acd5a8ea105e7362075a
Semere Habtu <se.semere.habtu@gmail.com>
2021-06-01 19:08:17 +0300

    Bonus cut get request

--- ems/bonus_cuts.py
+++ ems/bonus_cuts.py
@@ -1,5 +1,7 @@
 from flask import request, jsonify
 from flask_restful import Resource, abort
+from ems.models import BonusCuts
+from ems.auth import db
 
 class BonusCutsAPI(Resource):
     def post(self):
@@ -13,6 +15,19 @@ class BonusCutsAPI(Resource):
             date = request.form['date']
             amount = request.form['amount']
             remark = request.form['remark']
+        
+        new_bonuscut = BonusCuts(employee_id=employee_id,date=date, amount=amount,remark=remark)
+        
+        db.session.add(new_bonuscut)
+        db.session.commit()
+
+    def get(self, bonus_id=None):
+        if bonus_id:
+            bonus = BonusCuts.query.filter_by(id=bonus_id).first()
+            return bonus
+        else:
+            bonus = BonusCuts.query.all()
+            return bonus
 
             
     
