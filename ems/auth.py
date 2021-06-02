7433c8d32027cdffda11838d2c8dd4f7fcc5cd0a
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-06-02 23:54:18 +0300

    Added jwt verfication

--- ems/auth.py
+++ ems/auth.py
@@ -22,7 +22,24 @@ def login():
         if not user:
             return abort(404, "Invalid credentials")
     
-        
+        if bcrypt.check_password_hash(user.password, auth.password):
+            if user.user_role == "admin":
+                token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm="HS256")
+                # token = jwt.encode({'id' : user.id}, app.config['SECRET_KEY'], algorithm="HS256")
+                # login_user(user)
+                session["type"] = user.user_role
+                # return jsonify({'message': "admin login successful"})
+                response = jsonify({"username":user.username, "role":user.user_role,"token":token})
+                return response
+                
+            elif user.user_role == "manager":
+                token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm="HS256")
+                # token = jwt.encode({'id' : user.id}, app.config['SECRET_KEY'], algorithm="HS256")
+                # login_user(user)
+                session["type"] = user.user_role
+                # return jsonify({'message': "hr login successful"})
+                response = jsonify({"username":user.username, "role":user.user_role,"token":token})
+                return response
         else:
             return abort(404, "Invalid credentials")
             
