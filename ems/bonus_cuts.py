7d556f782c8ca4ac12cb4218b0afc755946ee301
Semere Habtu <se.semere.habtu@gmail.com>
2021-06-02 11:00:18 +0300

    Used schemas to serialize request

--- ems/bonus_cuts.py
+++ ems/bonus_cuts.py
@@ -1,8 +1,8 @@
 from flask import request, jsonify
 from flask_restful import Resource, abort
 from ems.models import BonusCuts
+from ems.schemas import bonus_schema, bonuss_schema
 from ems.auth import db
-
 class BonusCutsAPI(Resource):
     def post(self):
         if request.is_json:
@@ -15,21 +15,48 @@ class BonusCutsAPI(Resource):
             date = request.form['date']
             amount = request.form['amount']
             remark = request.form['remark']
+
+        # bonus = BonusCuts.query.filter_by(employee_id=employee_id).first()
         
         new_bonuscut = BonusCuts(employee_id=employee_id,date=date, amount=amount,remark=remark)
-        
+            
         db.session.add(new_bonuscut)
         db.session.commit()
+        
+        result = bonus_schema.dump(new_bonuscut)
+        print("Finished Registering bonus: ", result)
+        response = jsonify(result)
+        response.status_code = 201
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
     def get(self, bonus_id=None):
         if bonus_id:
             bonus = BonusCuts.query.filter_by(id=bonus_id).first()
-            return bonus
+            if bonus:
+                result = bonus_schema.dump(bonus)
+                response = jsonify(result)
+                response.status_code = 201
+                return response
+
+            else:
+                abort(404, "No Bonus/Cut found")
         else:
             bonus = BonusCuts.query.all()
-            return bonus
-    
+            if bonus:
+                result = bonuss_schema.dump(bonus)
+                response = jsonify(result)
+                response.status_code = 201
+                return response
+            else:
+                abort(404, "No Bonuses/Cuts found")
+
     def put(self, bonus_id):
+        # try:
+        #     bonus_schema.load(request.json)
+        # except:
+        #     abort(400, message="Invalid Request")
+            
         bonus_cuts = BonusCuts.query.filter_by(id=bonus_id).first()
         if bonus_cuts:
             if request.is_json:
@@ -51,8 +78,14 @@ class BonusCutsAPI(Resource):
 
             db.session.commit()
 
+            result = bonus_schema.dump(bonus_cuts)
+            response = jsonify(result)
+            response.status_code = 201
+            return response
+
         else:
             abort(404, "No bonus/cut with that Id")
+
 
     def delete(self, bonus_id):
         bonus_cuts = BonusCuts.query.filter_by(id=bonus_id).first()
@@ -62,6 +95,3 @@ class BonusCutsAPI(Resource):
             return jsonify(message="Bonus/Cut entry successfully deleted"), 200
         else:
             abort(404, "No bonus/cuts with that Id")
-
-            
-    
