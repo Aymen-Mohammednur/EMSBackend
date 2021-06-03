b3a0dc5789ae8444b8382d7725c0af844960ba03
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-06-03 17:04:19 +0300

    delete employee done

--- ems/employee.py
+++ ems/employee.py
@@ -80,3 +80,55 @@ class EmployeeAPI(Resource):
                 return response
             else:
                 abort(404, message="No employees found")
+
+    def put(self, employee_id):
+        # try:
+        #     employee_schema.load(request.json)
+        # except:
+        #     abort(400, message="Invalid request")
+
+        employee = Employee.query.filter_by(id=employee_id).first()
+        if employee:
+            if request.is_json:
+                first_name = request.json['first_name']
+                last_name = request.json['last_name']
+                email = request.json['email']
+                date_of_birth = request.json['date_of_birth']
+                hourly_rate = request.json['hourly_rate']
+                department_id = request.json['department_id']
+            else:
+                first_name = request.form['first_name']
+                last_name = request.form['last_name']
+                email = request.form['email']
+                date_of_birth = request.form['date_of_birth']
+                hourly_rate = request.form['hourly_rate']
+                department_id = request.form['department_id']
+            
+            employee.first_name=first_name
+            employee.last_name=last_name
+            employee.email=email
+            employee.date_of_birth=date_of_birth
+            employee.hourly_rate=hourly_rate
+            employee.department_id=department_id
+
+            db.session.commit()
+            
+            result = employee_schema.dump(employee)
+            response = jsonify(result)
+            response.status_code = 201
+            return response
+        
+        else:
+            abort(404, message="No employee with that Id")
+
+
+    def delete(self, employee_id):
+        employee = Employee.query.filter_by(id=employee_id).first()
+        if employee:
+            db.session.delete(employee)
+            db.session.commit()
+            response = jsonify({"message":"Employee successfully deleted"})
+            response.status_code = 202
+            return response
+        else:
+            abort(404, message="No employee with that Id")
